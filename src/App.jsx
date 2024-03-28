fed7c71f3e7a78f11d45a9c8ff7320efcbf46d42
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-03-28 23:19:25 +0100

    components2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import PockemonCard from "./components/PokemonCard";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 function App() {
   return (
     <>
-      <PockemonCard pokemon={pokemonList[0]} />
+      <PockemonCard />
     </>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,23 @@
-function PockemonCard({pokemon}) {
-  
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
+function PockemonCard() {
+  const pokemon = pokemonList[0];
+  
   return (
     
     <figure>
       {pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p/> ??? <p/>}
       <figcaption>{pokemon.name}</figcaption>
-      </figure>
+    </figure>
    );   
 }
 
