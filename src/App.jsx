2eb599c9a7cbd2cb9274c6f4963d5390903fb3a0
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-03-29 12:17:21 +0100

    mew

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import PockemonCard from "./components/PokemonCard";
-
+import PokemonCard from "./components/PokemonCard";
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
   return (
     <>
-      <PockemonCard />
+      <div className="App">
+        <PokemonCard pokemon={pokemonList[0]} />
+      </div>
     </>
   );
 }
